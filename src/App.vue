b48c44032c383e4998930438a3dbdfd18da6c016
sharif1292 <sharifhossaion57@gmail.com>
2024-01-16 23:06:00 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Todo from './components/Todo.vue'
+</script>
+
+<template>
+<todo/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,46 @@
+<script setup>
+    import {ref} from 'vue';
+    const tokens = ref([
+            { name: "Task-1", time: "1" },
+            { name: "Task-2", time: "2" }
+        ]);
+    function addNewText(){
+        const newTask = {
+            time: tokens.value.length + 1,
+            name: `Task - ${tokens.value.length+ 1}`
+        };
+        tokens.value.push(newTask);
+    }
+    function deleteTask(i){
+        if(confirm("Are You Sure ?")){
+            tokens.value.splice(i, 1);
+        }
+    }
+</script>
+<template>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Time</th>
+                <th>Acction</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(token,i) in tokens">
+                <th>{{ token.name }}</th>
+                <td>{{ token.time+" min" }}</td>
+                <td>
+                    <a href="" @click.prevent="deleteTask(i)" class="btn btn-danger">Remove</a>
+                </td>
+            </tr>
+        </tbody>
+
+    </table>
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <button @click="addNewText()" class="btn btn-primary" type="button"> New Task</button>
+        </div>
+</template>
+<style scoped>
+
+</style>
